e6035679c58e6033fffba76bc955b27e326f295b
Arturo Calderon <calderov@gmail.com>
2024-09-29 22:39:12 -0700

    Move dashboard page to overview and add loading screen

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,7 @@
 import { Card } from '@/app/ui/dashboard/cards';
-import RevenueChart from '../ui/dashboard/revenue-chart';
-import LatestInvoices from '../ui/dashboard/latest-invoices';
-import { lusitana } from '../ui/fonts';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 
 export default async function Page() {
